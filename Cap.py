4e9491890184b16d00cf7cdcac6616d005ee2f58
n-saud <mew959595@gmail.com>
2020-02-22 14:58:56 -0500

    Webcam Capture Initial

---
+++ Cap.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+Image = cv2.VideoCapture(0)
+Key = cv2.waitKey(1)
+
+while(True):
+    ret, frame = Image.read()
+    cv2.imshow("Smile",frame)
+    cv2.imwrite('abs.jpg', frame)
+    Image.release()
+    break
+
+cv2.destroyAllWindows()
