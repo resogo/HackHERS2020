51457db542d9106b9465c8a654fd5ad053ae2ba2
Rebecca <rebeccagolm32@gmail.com>
2020-02-23 09:43:34 -0500

    finishing touches

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ emotions = ["anger", "joy", "surprise", "sorrow"]
 cv2.rectangle(result, (90,550), (800, 850), (255,255,255), cv2.FILLED)
 for i in range(len(emotion_ratings)):
     cv2.putText(result,emotions[i]+" : "+emotion_ratings[i],(100,600+70*i), font, 3,(0,0,0),5,cv2.LINE_AA)
-
+cv2.imwrite('./media/result.jpg', result)
 hero = cv2.imread(superhero_file, -1);
 cv2.namedWindow("Smile", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("Smile",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
